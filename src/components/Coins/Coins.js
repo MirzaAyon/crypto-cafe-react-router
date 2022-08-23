963bef3fefc9702f072158bddd384ad3e13705a1
MirzaAyon <ayonmirza98@gmail.com>
2022-08-23 12:26:13 +0600

    50 datas are showing

--- src/components/Coins/Coins.js
+++ src/components/Coins/Coins.js
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Coins = () => {
+    const [coins, setCoins] = useState([]);
+    useEffect(() => {
+        fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false')
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                setCoins(data)
+            })
+
+        //data take console log kore dekhbo
+        //50 ta data load hbe 
+        //perpage =50 ke 100 banae dbo tokhn 100 ta data load hbe 
+    }, [])
     return (
         <div>
             <h2>Coins are here </h2>
