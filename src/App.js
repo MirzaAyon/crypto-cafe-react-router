60276a584e83603aa3679933476eebb0e552bbc7
MirzaAyon <ayonmirza98@gmail.com>
2022-08-23 09:29:34 +0600

    coins page updated and coin nav working

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Header from './components/Header/Header'
 import Homepage from './components/Homepage/Homepage'
+import Coins from './components/Coins/Coins'
 
 const App = () => {
   return (
@@ -11,6 +12,8 @@ const App = () => {
       {/* header ekhane karon every page e eke dekhate chai  */}
 
       <Routes>
+        <Route path='/' element={<Homepage></Homepage>}></Route>
+        <Route path='/coins' element={<Coins></Coins>}></Route>
         <Route path='/' element={<Homepage></Homepage>}></Route>
       </Routes>
     </div>
