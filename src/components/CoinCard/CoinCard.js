af18875614e0ef5df4f71c4e0a068c7a35094857
MirzaAyon <ayonmirza98@gmail.com>
2022-08-23 14:35:53 +0600

    all coin are showing as cards with name and id

--- src/components/CoinCard/CoinCard.js
+++ src/components/CoinCard/CoinCard.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const CoinCard = ({ coin }) => {
     return (
-        <div>
-            <p>{coin.id}</p>
-            <p>{coin.name}</p>
+        <div className='shadow-lg rounded-2xl w-[250px] bg-white p-4'>
+            {/* <p>{coin.id}</p>
+            <p>{coin.name}</p> */}
+            <Link to='/coins' >
+                <div className='gap-4 flex justify-between items-center'>
+                    <div className='flex-shrink-0'>
+                        {/* shirnk flex er maddhome img chooto boro hy  */}
+
+                        <div>
+                            <img
+                                alt='profile'
+                                src={coin.image}
+                                className='mx-auto object-cover rounded-full h-16 w-16 '
+                            />
+                        </div>
+
+
+                    </div>
+                    <div className=' flex flex-col justify-end'>
+                        <span className='text-gray-600 font-medium'>{coin.name}</span>
+                        <span className='text-gray-400 text-xs'>{coin.symbol}</span>
+                    </div>
+
+                </div>
+            </Link>
+
+
         </div>
     );
 };
