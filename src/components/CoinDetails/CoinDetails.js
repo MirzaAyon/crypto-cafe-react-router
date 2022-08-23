801fbfc7d93dd25649a4c3ec15271bd7584c9e9e
MirzaAyon <ayonmirza98@gmail.com>
2022-08-23 15:40:20 +0600

    id parameter get by useParams hook in coinDetails component

--- src/components/CoinDetails/CoinDetails.js
+++ src/components/CoinDetails/CoinDetails.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const CoinDetails = () => {
+    const { id } = useParams();
     return (
         <div>
             <h2>Coin details</h2>
