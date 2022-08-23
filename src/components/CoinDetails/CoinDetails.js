7ffa551f5609a2058a0a8ee1bd7909db2409cf8b
MirzaAyon <ayonmirza98@gmail.com>
2022-08-23 15:54:48 +0600

    after clicking coins individual coins are shown in inspact

--- src/components/CoinDetails/CoinDetails.js
+++ src/components/CoinDetails/CoinDetails.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const CoinDetails = () => {
     const { id } = useParams();
+    const [coin, setCoin] = useState({});
+    useEffect(() => {
+        const url = `https://api.coingecko.com/api/v3/coins/${id}`
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data); //eta just dekhar  jnno click korle showing
+            })
+        //id ta change hole code ta abr run hbe tai dependancy dbo
+        //ekhn id joto bar change hbe uporer code toto bar call hbe 
+    }, [id])
     return (
-        <div>
-            <h2>Coin details</h2>
+        <div className='px-4 pt-20 pb-24 mx-auto max-w-7xl md:px-2'>
+            {/* <h2>Coin details</h2> */}
         </div>
     );
 };
