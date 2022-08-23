0a480510ccec047bf06e4212ae7e48bf99dd8dae
MirzaAyon <ayonmirza98@gmail.com>
2022-08-23 09:35:01 +0600

    header done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div className='sticky top-0 bg-white px-12 py-8 flex justify-center md:justify-between'>
+        <div className='sticky top-0  bg-white px-12 py-8 flex justify-center md:justify-between'>
             <div className='hidden md:block'>
                 <Link className='font-sans font-bold text-xl text-gray-600' to='/'>
                     Crypto Cafe <span className='text-xl'>☕</span>
