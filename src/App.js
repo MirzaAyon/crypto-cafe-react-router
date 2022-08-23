8fd9ca4109708729215502b59669eca844728cbc
MirzaAyon <ayonmirza98@gmail.com>
2022-08-23 09:32:33 +0600

    contact route updated

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom'
 import Header from './components/Header/Header'
 import Homepage from './components/Homepage/Homepage'
 import Coins from './components/Coins/Coins'
+import Contact from './components/Contact/Contact'
 
 const App = () => {
   return (
@@ -14,7 +15,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Homepage></Homepage>}></Route>
         <Route path='/coins' element={<Coins></Coins>}></Route>
-        <Route path='/' element={<Homepage></Homepage>}></Route>
+        <Route path='/contact' element={<Contact></Contact>}></Route>
       </Routes>
     </div>
   )
